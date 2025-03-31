7abc867bdf91a7563491314a16fe86b09bfa7241
SayHoang <sayhoang.work@gmail.com>
2025-03-31 09:32:49 +0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()
   ],
   base: '/intro_monkey5/',
   server: {
